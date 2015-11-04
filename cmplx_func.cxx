642de939cb26beb1ca2c090b8c9c29eef2ac298d
Goetz Lehmann <goetz@tp1.uni-duesseldorf.de>
2015-11-04 12:03:44 +0100

    added source files

---
+++ cmplx_func.cxx
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <complex>
+
+int main(){
+
+  // Example how to declare a single complex variable:
+  complex<double> c;
+
+  // d = 1 + 2i 
+  complex<double> d = complex<double>(1.0, 2.0); 
+
+  return 0;
+}
